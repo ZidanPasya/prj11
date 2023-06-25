9ca88124ee7d63e97b48745718b15e530ade5fa8
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2023-06-25 22:54:55 +0700

    banyak

--- form/signIn.php
+++ form/signIn.php
@@ -1,29 +1,28 @@
-<?php 
-  require "../functions.php";
+<?php
+require "../functions.php";
 
-  session_start();
 
-  if(isset($_SESSION["login"])){
-    header("Location: ../index.php");
-    exit;
-  }
+if (isset($_SESSION["login"])) {
+  header("Location: ../index.php");
+  exit;
+}
 
-  if(isset($_POST['submit'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+if (isset($_POST['submit'])) {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
 
-    $result = mysqli_query($conn, "SELECT * FROM peserta WHERE email = '$email'");
-
-    if(mysqli_num_rows($result) === 1){
-      $row = mysqli_fetch_assoc($result);
-      if(password_verify($password, $row['password'])){
-        $_SESSION["login"] = true;
-
-        header("Location: ../index.php");
-        exit;
-      }
+  $result = mysqli_query($conn, "SELECT * FROM peserta WHERE email = '$email'");
+  var_dump($result);
+  if (mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    if (password_verify($password, $row['password'])) {
+      $_SESSION["login"] = true;
+      $_SESSION["id"] = $row["id"];
+      header("Location: ../index.php");
+      exit;
     }
   }
+}
 ?>
 
 <!DOCTYPE html>
--- functions.php
+++ functions.php
@@ -1,34 +1,39 @@
-<?php 
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "prj";
-    $conn = mysqli_connect($host, $user, $pass, $db);
+<?php
 
-    if(mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . $conn->connect_error;
-        exit();
-    }
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "prj";
+$conn = mysqli_connect($host, $user, $pass, $db);
 
-    function register($data){
-        global $conn;
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
+    exit();
+}
+if (!isset($_SESSION)) {
+    session_start();
+}
 
-        $email = $data['email'];
-        $name = $data['name'];
-        $telpon = $data['telpon'];
-        $password = mysqli_real_escape_string($conn, $data['password']);
-        $confirmpassword = mysqli_real_escape_string($conn, $data['confpass']);
+function register($data)
+{
+    global $conn;
 
-        if($password !== $confirmpassword){
-            echo "<script>
+    $email = $data['email'];
+    $name = $data['name'];
+    $telpon = $data['telpon'];
+    $password = mysqli_real_escape_string($conn, $data['password']);
+    $confirmpassword = mysqli_real_escape_string($conn, $data['confpass']);
+
+    if ($password !== $confirmpassword) {
+        echo "<script>
                     alert('Password tidak sesuai');
                   </script>";
-            return false;
-        }
-
-        $password = password_hash($password, PASSWORD_DEFAULT);
-
-        mysqli_query($conn, "INSERT INTO peserta(id_peserta, email, nama, notelp, password) 
-                    VALUES (NULL, '$email', '$name', '$telpon', '$password')");
+        return false;
     }
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    mysqli_query($conn, "INSERT INTO peserta(id, email, nama, telpon, password) 
+                    VALUES (NULL, '$email', '$name', '$telpon', '$password')");
+}
 ?>
