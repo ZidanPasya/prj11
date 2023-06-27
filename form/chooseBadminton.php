7241ba784c270e421a9b26a958aa7eb57b2381d7
Kira <kira68784@gmail.com>
2023-06-27 10:48:17 +0800

    fix wrong favicon

--- form/chooseBadminton.php
+++ form/chooseBadminton.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8" />
-  <link href="img/Logo.png" rel="icon" />
+  <link href="../assets/img/Logo.png" rel="icon" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Daftar Badminton | PRJ x HT XI</title>
--- form/signIn.php
+++ form/signIn.php
@@ -29,7 +29,7 @@ if (isset($_POST['submit'])) {
 
 <head>
   <meta charset="UTF-8" />
-  <link href="img/Logo.png" rel="icon" />
+  <link href="../assets/img/Logo.png" rel="icon" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Sign In | PRJ x HT XI</title>
