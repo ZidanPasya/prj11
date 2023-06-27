b685685e9e89bf4d93fc1d0e6dbf44a7dc77edfe
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2023-06-28 00:20:39 +0700

    folder

--- form/chooseBadminton.php
+++ form/chooseBadminton.php
@@ -3,39 +3,40 @@
 
 
 <head>
-  <meta charset="UTF-8" />
-  <link href="img/Logo.png" rel="icon" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Daftar Badminton | PRJ x HT XI</title>
-  <meta content="Daftar Badminton PRJ x HT XI Ignite : Unleashing the Spark of Youth Potential" name="description" />
-  <meta content="PRJxHT, PRJxHT 11, Daftar, Badminton " name="keywords" />
+    <meta charset="UTF-8" />
+    <link href="img/Logo.png" rel="icon" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Daftar Badminton | PRJ x HT XI</title>
+    <meta content="Daftar Badminton PRJ x HT XI Ignite : Unleashing the Spark of Youth Potential" name="description" />
+    <meta content="PRJxHT, PRJxHT 11, Daftar, Badminton " name="keywords" />
 
-  <link rel="stylesheet" href="css/form.css" />
-  <link rel="stylesheet" href="css/style.css" />
-  <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/form.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
 
-  <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
+        rel="stylesheet" />
 </head>
 
 <body>
-  <div class="row">
-    <div class="col-md-12">
-      <form>
-        <h1>Pilih Kategori</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <form>
+                <h1>Pilih Kategori</h1>
 
-        <button onclick="location.href='soloBadminton.php'" type="button" class="btn-buat">
-          Tunggal Putra
-        </button>
-        <button onclick="location.href='mixedBadminton.php'" type="button" class="btn-buat">
-          Ganda Campuran
-        </button>
-        <button onclick="javascript:history.back()" type="button" class="btn-danger mt-3"><span
-            class="text-light">Back</span></button>
-      </form>
+                <button onclick="location.href='soloBadminton.php'" type="button" class="btn-buat">
+                    Tunggal Putra
+                </button>
+                <button onclick="location.href='mixedBadminton.php'" type="button" class="btn-buat">
+                    Ganda Campuran
+                </button>
+                <button onclick="javascript:history.back()" type="button" class="btn-danger mt-3"><span
+                        class="text-light">Back</span></button>
+            </form>
 
+        </div>
     </div>
-  </div>
 </body>
 
 </html>
