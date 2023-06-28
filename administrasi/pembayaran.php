b469bc3bbcd2bc28b7f0e6e2f3592811a6f68b38
Iqbalalh <iqbal.al21@students.unila.ac.id>
2023-06-28 13:14:34 +0700

    lomba

--- administrasi/pembayaran.php
+++ administrasi/pembayaran.php
@@ -107,7 +107,7 @@ $datas = mysqli_fetch_assoc($queri1);
                         </a>
                     </li>
                 </ul>
-                <ul>
+                <!-- <ul>
                     <li class="relative px-6 py-3">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                             href="lomba.php">
@@ -120,7 +120,7 @@ $datas = mysqli_fetch_assoc($queri1);
                             <span class="ml-4">Lomba</span>
                         </a>
                     </li>
-                </ul>
+                </ul> -->
                 <ul>
                     <li class="relative px-6 py-3">
                         <span class="absolute inset-y-0 left-0 w-1 bg-yellow-600 rounded-tr-lg rounded-br-lg"
