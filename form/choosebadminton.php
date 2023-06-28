a29a934b9d059f5a6aaa4eb36e18303c6506d9bb
Faiz Muzaki <34159640+faizmuzaki@users.noreply.github.com>
2023-06-28 16:57:15 +0700

    feat: fix url

---
+++ form/choosebadminton.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8" />
+    <link href="img/Logo.png" rel="icon" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Daftar Badminton | PRJ x HT XI</title>
+    <meta content="Daftar Badminton PRJ x HT XI Ignite : Unleashing the Spark of Youth Potential" name="description" />
+    <meta content="PRJxHT, PRJxHT 11, Daftar, Badminton " name="keywords" />
+
+    <link rel="stylesheet" href="css/form.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
+        rel="stylesheet" />
+</head>
+
+<body>
+    <div class="bg-badmin">
+    <div class="row">
+        <div class="col">
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+
+            <form class="formm">
+                <h1 style="color: white">Pilih Kategori</h1>
+
+                <button onclick="location.href='soloBadminton.php'" type="button" class="btn-buat">
+                    Tunggal Putra
+                </button>
+                <button onclick="location.href='mixedBadminton.php'" type="button" class="btn-buat">
+                    Ganda Campuran
+                </button>
+                <button onclick="javascript:history.back()" type="button" class="btn-danger mt-3"><span
+                        class="text-light">Back</span></button>
+            </form>
+
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
---
+++ form/cpc.php
@@ -0,0 +1,83 @@
+<?php
+require "../functions.php";
+if (!$_SESSION["login"]) {
+  header("location:signIn.php");
+}
+$id = $_SESSION["id"];
+$queri = mysqli_query($conn, "SELECT * FROM peserta WHERE id = '$id'");
+$data = mysqli_fetch_assoc($queri);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link href="img/Logo.png" rel="icon" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Daftar CPC | PRJ x HT XI</title>
+  <meta
+    content="Daftar Competitive Programming Competition PRJ x HT XI Ignite : Unleashing the Spark of Youth Potential"
+    name="description" />
+  <meta content="PRJxHT, PRJxHT 11, Daftar, CPC, Competitive Programming Competition" name="keywords" />
+
+  <link rel="stylesheet" href="css/form.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+  <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="row bg-cpc">
+    <div class="col-0 col-md-3"></div>
+    <div class="col-12 col-md-6 mx-auto">
+      <form action="action/cpcAct" method="post" enctype="multipart/form-data" class="formm">
+        <h1 style="color: white;">Daftar Competitive Programming Competition</h1>
+
+        <fieldset class="mb-3">
+          <legend><span class="number">1</span> Profil</legend>
+
+          <label for="username">Email</label>
+          <input required type="email" id="username" name="username" value="<?= $data['email'] ?>" />
+
+          <label for="name">Nama Lengkap</label>
+          <input required type="text" id="name" name="name" value="<?= $data['nama'] ?>" />
+
+          <label for="telp">No.Telpon</label>
+          <input required type="text" id="telp" name="telp" value="<?= $data['telpon'] ?>" />
+
+          <label for="school">Instansi</label>
+          <input required type="text" id="school" name="school" />
+
+          <label for="birth">Tanggal lahir</label>
+          <input required type="date" id="birth" name="birth" />
+
+          <label for="gambar">
+            Bukti Identitas (Kartu Pelajar / Halaman Depan Rapot)
+          </label>
+          <br>
+          <small id="max" class="form-text text-muted">Max 1MB</small>
+          <div class="input-group">
+            <input required type="file" class="form-control mb-3" id="gambar" name="gambar"
+              accept="image/x-png,image/gif,image/jpeg" />
+          </div>
+
+        </fieldset>
+        <div class="row">
+          <div class="col">
+            <button onclick="javascript:history.back()" type="button" class="btn-danger"><span
+                class="text-light">Back</span></button>
+          </div>
+          <div class="col">
+            <button type="submit">Daftar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-0 col-md-3"></div>
+  </div>
+</body>
+
+</html>
